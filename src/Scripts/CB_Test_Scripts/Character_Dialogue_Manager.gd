b1c99ef8ff491c392dcc18e968578237134508cb
Christian Bortolotti <cbortolo@purdue.edu>
2020-09-20 17:47:32 -0400

    Dialogue Playground
    - Created basic Node hierarchy for an instanced DialogueEngine scene and the DialoguePlayground scene
    - Basic Character script with an exported spID var, hopefully the only manual aspect of the dialogue importing process
    - DialogueEngine scripting framework with a method that receives transmissions from characters

---
+++ src/Scripts/CB_Test_Scripts/Character_Dialogue_Manager.gd
@@ -0,0 +1,14 @@
+extends Node
+
+# member variables
+export var speakerID = "setMe!"
+onready var interactable = get_node("Button")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	interactable.connect("pressed", self, "_interacts")
+
+# Call the Dialogue Manager using the SpeakerID
+func _interacts():
+	print("SpeakerID: " + speakerID)
+	get_node("../DialogueEngine")._beginTransmit(speakerID)
