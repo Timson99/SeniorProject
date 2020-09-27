1327cc17cbc488b7f6f7a42cb0fffb609a8eefe7
Joe <nintenjoe98@gmail.com>
2020-09-27 13:08:05 -0700

    Attempt to resolve merge issues

--- src/Scripts/Singletons/SceneManager.gd
+++ src/Scripts/Singletons/SceneManager.gd
@@ -13,20 +13,13 @@ func _ready():
 	current_scene = root.get_child(root.get_child_count() - 1)
 	
 # Call this function from anywhere to change scene
-# Example: SceneManager.goto_scene(path_string) 
-func goto_scene(path, warp_destination_id): 
-	#############
-	emit_signal("goto_called", path)
-	#GameManager.input_paused = true
-	#SaveManager.update_persistant_data() #Call Update Data
-	#############
+# Example: SceneManager.goto_scene(path_string, warp_destination_id) 
+func goto_scene(path, warp_destination_id = ""): 
+	emit_signal("goto_called")
 	call_deferred("_deferred_goto_scene", path, warp_destination_id)
 
 # Switches Scenes only when it is safe to do so
 func _deferred_goto_scene(path, warp_destination_id):
-	
-	print(path)
-	 
 	var fade = fade_screen.instance()
 	get_tree().get_root().add_child(fade)
 	var fade_animation = fade.get_node("TextureRect/AnimationPlayer")
@@ -38,14 +31,16 @@ func _deferred_goto_scene(path, warp_destination_id):
 	var s = ResourceLoader.load(path) # Load the new scene.
 	current_scene = s.instance() # Instance the new scene.
 	get_tree().get_root().add_child(current_scene) # Add as child of root
-	##########
 	emit_signal("scene_loaded")
-	#PersistantData.restore_data() #Call Restore Data to Nodes
-	##########
-	if warp_destination_id < 0 && current_scene.get_node("Map/%s" % warp_destination_id) && current_scene.get_node("Map/Player"):
-		print("Id Exists")
-		var warp_node = current_scene.get_node("Map/%s" % warp_destination_id)
-		current_scene.get_node("Map/Player").reposition(warp_node.entrance_point, warp_node.entrance_direction)
+	var warps : Array = get_tree().get_nodes_in_group("Warp")
+	var party : Array = get_tree().get_nodes_in_group("Party")
+	
+	if (warp_destination_id != "" && party.size() == 1 && warps.size() >= 1):
+		for warp in warps:
+			if ("warp_destination_id" in warp && 
+			warp.warp_id == warp_destination_id):	
+				party[0].reposition(warp.entrance_point, warp.entrance_direction)
+				break
 	
 	fade_animation.play_backwards("Fade")
 	yield(fade_animation, "animation_finished")
@@ -54,6 +49,3 @@ func _deferred_goto_scene(path, warp_destination_id):
 	emit_signal("scene_fully_loaded")
 	
 	
-	
-
-
