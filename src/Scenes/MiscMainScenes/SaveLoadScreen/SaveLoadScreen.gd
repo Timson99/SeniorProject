df44c39b7783e888ca7047b6a852ab340205c843
Tim Herrmann <tvherrmann@comcast.net>
2020-10-24 16:43:11 -0700

    Remove Redundancies

--- src/Scenes/MiscMainScenes/SaveLoadScreen/SaveLoadScreen.gd
+++ src/Scenes/MiscMainScenes/SaveLoadScreen/SaveLoadScreen.gd
@@ -10,9 +10,8 @@ var buttons = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	"""
 	var save_file_names = SaveManager.save_files
-	save_files_num = save_file_names.size()
+	"""
 	for file_name in save_file_names:
 		var save_file = save_file_scene.instance()
 		save_file.save_file_name = file_name
@@ -35,8 +34,6 @@ func down():
 	focused += 1
 	focused = min(save_files_num, focused)
 	buttons[focused-1].get_node("AnimatedSprite").animation = "on" 
-	print("Save Files Num %s:" % save_files_num)
-	print("Focused %s:" % focused)
 	if(focused > 3):
 		var min_y = 32 + ((save_files_num - 3) * (-64))
 		button_container.rect_position.y = max(button_container.rect_position.y-64,min_y)
